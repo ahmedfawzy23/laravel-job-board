79c716b674b66335b22616f46e79d5eb26b46482
Ahmed Fawzy <ahmedfawzy169236@gmail.com>
2025-06-08 13:49:14 +0300

    Blade Templating Engine Module

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<x-layout :title="$pageTitle">
+    <h1>About Page</h1>
+</x-layout>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+
+    <h1>welcome to job Board</h1>
+
+    <ul>
+        <li>jobs of today</li>
+        <li>jobs trinding</li>
+    </ul>
+</x-layout>
